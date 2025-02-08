5c5f891d872914abfe2d49f6ef04b266a9673326
vadimBRR <vadmabos20@gmail.com>
2025-02-08 15:31:02 +0100

    add profile page with orders list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import { Toaster } from 'sonner'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route index element={<Home/>}/>
           <Route path='login' element={<Login/>}/>
           <Route path='register' element={<Register/>}/>
+          <Route path='profile' element={<Profile/>}/>
         </Route>
         <Route>
           {/* Admin */}
--- frontend/src/components/Products/ProductDetails.tsx
+++ frontend/src/components/Products/ProductDetails.tsx
@@ -119,8 +119,8 @@ const ProductDetails = () => {
     </div>
   }
 	return (
-		<div className='p-6'>
-			<div className='max-w-6xl mx-auto bg-white p-8 rounded-lg'>
+		<div className='p-0 md:p-6'>
+			<div className='max-w-6xl mx-auto bg-white  rounded-lg'>
 				<div className='flex flex-col md:flex-row'>
 					{/* Left */}
 					<div className='hidden md:flex flex-col space-y-4 mr-6'>
